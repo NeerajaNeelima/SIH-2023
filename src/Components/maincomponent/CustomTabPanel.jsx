a5763334499ae6e10640398dabf24192bd0d7d0b
sriharika <sudha200215@gmail.com>
2023-10-07 00:29:58 +0530

    Tabs 2 and 3 added

--- src/Components/maincomponent/CustomTabPanel.jsx
+++ src/Components/maincomponent/CustomTabPanel.jsx
@@ -32,9 +32,146 @@ const CustomTabPanel = () => {
 
         </Box>
         
-        <TabPanel value="1" className='tab'><img src={map} width='1100rem' height='550rem' style={{marginLeft:'-8rem',padding:'0px'}} /></TabPanel>
-        <TabPanel value="2"  className='tab'>Panel Two</TabPanel>
-        <TabPanel value="3"  className='tab'>Panel Three</TabPanel>
+        <TabPanel value="1" className='tab'><img src={map} width='1100rem' height='550rem' style={{marginLeft:'-2rem',padding:'0px'}} /></TabPanel>
+        <TabPanel value="2"  className='tab'>
+
+          <p>Ladle No. or Station No.</p>
+          <div className='querybox'>
+            <input type='text' placeholder='     Ladle Number or Station Number' /><br/>
+            <button> Search</button>
+          </div> 
+          <p className='output' style={{}}>
+          <b >FURNACE 1</b>
+          </p> 
+          <table>
+            <tr>
+              <th>Ladle Type</th>
+              <th>Ladle Number</th>
+              <th>position</th>
+              <th>Time stamp</th>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+            </tr>
+          </table>
+
+        </TabPanel>
+        <TabPanel value="3"  className='tab'>
+
+        <table style={{marginLeft:'0px'}}>
+            <tr>
+              <th>Ladle Type</th>
+              <th>Ladle Number</th>
+              <th>position</th>
+              <th>Time stamp</th>
+              <th>TAT</th>
+              <th>CT</th>
+              <th>LT</th>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>Hot metal ladle</td>
+              <td>H5</td>
+              <td>FURNACE 1 </td>
+              <td>23:15:06 7th October 2023 </td>
+              <td>150</td>
+              <td>4</td>
+              <td>8</td>
+            </tr>
+          </table>
+        </TabPanel>
         <TabPanel value="4"  className='tab'>Panel Four</TabPanel>
       </TabContext>
     </Box>
