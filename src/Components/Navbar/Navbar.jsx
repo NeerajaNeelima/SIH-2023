c4b2114782a42617e5896b8ff1058185a93d9da2
NeerajaNeelima <023neeraja@gmail.com>
2023-10-06 10:53:21 +0530

    Navbar added

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import LoginIcon from '@mui/icons-material/Login';
+import image from '../../assets/steel_plant_logo.jpeg'
+function Navbar() {
+  return (
+    <nav className="navbar">
+        <div className="container">
+            <div className="logo">
+             <img src={image} alt='Steel-Plant-Logo'/>     
+            </div>
+            <div className="title">
+                  rastriya ispat nigam limited
+                  <p>visakhapatnam steel plant</p>
+                  <p>(A Govt. of India Enterprise)</p>
+            </div>
+            <div className='links' >
+                <ul className='ul-flex'>
+                    <li>
+                      <div className='nav-link'>
+                      <NavLink to="/sign-in"><span><HowToRegIcon/>Sign In</span></NavLink>
+                      </div>
+                    </li>
+                    <li>
+                        <div className='nav-link'>
+                          <NavLink to="/sign-up"><span> <LoginIcon/>Log In</span></NavLink>   
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
